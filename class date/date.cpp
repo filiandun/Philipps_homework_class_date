910492d217056b0105f6d692af6c83e08b914d13
filian <filiandun@gmail.com>
2022-12-29 13:09:01 +0300

    class Date (1): ready

--- class date/date.cpp
+++ class date/date.cpp
@@ -1,2 +1,124 @@
 #include "date.h"
 
+Date::Date()
+{
+}
+
+Date::Date(unsigned short int day, unsigned short int month, unsigned short int year)
+{
+	this->day = day;
+	this->month = month;
+	this->year = year;
+}
+
+Date::Date(const Date& d)
+{
+	this->day = d.day;
+	this->month = d.month;
+	this->year = d.year;
+}
+
+Date::~Date() {}
+
+
+
+Date& Date::operator++()
+{
+	++this->day;
+
+	return *this;
+}
+
+
+Date& Date::operator--()
+{
+	--this->day;
+
+	return *this;
+}
+
+
+Date Date::operator++(int)
+{
+	Date previous_date = *this;
+	++this->day;
+
+	return previous_date;
+}
+
+
+Date Date::operator--(int)
+{
+	Date previous_date = *this;
+	--this->day;
+
+	return previous_date;
+}
+
+
+
+
+bool Date::operator!=(const Date& d) const
+{
+	return (this->year * 365 + this->month * 30 + this->day) != (d.year * 365 + d.month * 30 + d.day);
+}
+
+
+bool Date::operator==(const Date& d) const
+{
+	return (this->year * 365 + this->month * 30 + this->day) == (d.year * 365 + d.month * 30 + d.day);;
+}
+
+
+bool Date::operator>(const Date& d) const
+{
+	return (this->year * 365 + this->month * 30 + this->day) > (d.year * 365 + d.month * 30 + d.day);
+}
+
+
+bool Date::operator<(const Date& d) const
+{
+	return (this->year * 365 + this->month * 30 + this->day) < (d.year * 365 + d.month * 30 + d.day);
+}
+
+
+
+
+Date& Date::operator=(const Date& d)
+{
+	this->day = d.day;
+	this->month = d.month;
+	this->year = d.year;
+
+	return *this;
+}
+
+
+Date& Date::operator+=(const Date& d)
+{
+	this->day += d.day;
+	this->month += d.month;
+	this->year += d.year;
+
+	return *this;
+}
+
+
+Date& Date::operator-=(const Date& d)
+{
+	this->day -= d.day;
+	this->month -= d.month;
+	this->year -= d.year;
+
+	return *this;
+}
+
+
+
+
+std::ostream& operator<<(std::ostream& output, const Date& d)
+{
+	output << d.day << "." << d.month << "." << d.year << std::endl;
+
+	return output;
+}
--- class date/date.h
+++ class date/date.h
@@ -1,7 +1,36 @@
 #pragma once
+#include <iostream>
 
 class Date
 {
+public:
+	Date();
+	Date(unsigned short int day, unsigned short int month, unsigned short int year);
+	Date(const Date& d);
+	~Date();
 
+private:
+	unsigned short int day = 0;
+	unsigned short int month = 0;
+	unsigned short int year = 0;
+
+public:
+	// ++, -- , != , == , > , <, >> , << , =, +=, -=, ()
+	Date& operator++();
+	Date& operator--();
+	Date operator++(int);
+	Date operator--(int);
+
+	bool operator!=(const Date& d) const;
+	bool operator==(const Date& d) const;
+
+	bool operator>(const Date& d) const;
+	bool operator<(const Date& d) const;
+
+	Date& operator=(const Date& d);
+	Date& operator+=(const Date& d);
+	Date& operator-=(const Date& d);
+
+	friend std::ostream& operator<<(std::ostream& output, const Date&d);
 };
 
